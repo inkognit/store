530962a0fd9981ed3c6c98cbf58213db8c300b76
shypileu_m <shypileu_m@lwo.by>
2022-11-17 17:51:10 +0300

    feat: settings
    master

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { Auth } from './configs/middlewares/auth.middleware';
+import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [AuthModule, UserModule],
   controllers: [],
   providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(Auth).forRoutes('');
+    consumer
+      .apply(Auth)
+      .exclude({ path: 'auth', method: RequestMethod.POST }, 'auth/(.*)')
+      .forRoutes('*');
   }
 }
