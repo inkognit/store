d5d53ce8f8c17462c4249bbf4ee9a62c2f962287
shypileu_m <shypileu_m@lwo.by>
2023-01-05 13:41:16 +0300

    feat: logs resource
    STORE-4

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from './configs/core/core';
 import { AuthModule } from './modules/auth/auth.module';
+import { LogsModule } from './modules/logs/logs.module';
 import { RolesModule } from './modules/roles/roles.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-    imports: [AuthModule, CoreModule, UserModule, RolesModule],
+    imports: [AuthModule, CoreModule, UserModule, RolesModule, LogsModule],
 })
 export class AppModule {}
--- src/db/db.sql
+++ src/db/db.sql
@@ -16,3 +16,9 @@ CREATE TABLE test.users(
 
 INSERT INTO test.users(login, password, first_name, middle_name, last_name, bithday, create_at, update_at) VALUES 
 ('string', '$argon2id$v=19$m=65536,t=3,p=4$anNmNWY0ZXIzZHM0ZjIoNTQyNTZocWFzZHU2KjIwcSMlIWhxKCozKCohJiVHRFdLSDIz$qy4lmIaX0JIfCs7h9Y2MExwMf5dBq5DIvmUwgjkhi9w', 'test', 'test', 'test', '1996-12-02 17:56:55.993732+03', '2022-11-18 10:56:55.993732+03', '2022-11-18 10:56:55.993732+03');
+
+CREATE TABLE test.logs(
+    data character varying NOT NULL,
+    ip character varying,
+    create_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+)
