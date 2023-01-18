23801d0658096dfdf9fadea4b45cfec2af9daeb0
shypileu_m <shypileu_m@lwo.by>
2023-01-18 10:17:21 +0300

    introduction of various configuration
    introduction compression as global and comment
    
    STORE-9

--- src/app-setup.ts
+++ src/app-setup.ts
@@ -1,10 +1,13 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import * as compression from 'compression';
 import * as cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './configs/filters/http-exeption-filters/http-exeption-filters.filter';
 
 export const setup = (app: INestApplication) => {
     app.use(cookieParser());
+    // app.use(compression()); // Необходим для сжатия больших объемов. Потому лучше не использовать где попало
+
     const config = new DocumentBuilder()
         .addBearerAuth()
         .setTitle('Example')
@@ -12,6 +15,7 @@ export const setup = (app: INestApplication) => {
         .setVersion('1.0')
         .addTag('Auth', 'APIs for authorize')
         .addTag('Users', 'APIs for Users test stores')
+        .addTag('Roles', 'APIs for CRUD roles')
         .build();
 
     app.useGlobalFilters(new HttpExceptionFilter());
