ba7654c5563c6205c02966e3e295738616895594
shypileu_m <shypileu_m@lwo.by>
2023-03-13 14:37:41 +0300

    feat: update to edit user pass
    master

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,34 +1,18 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsDateString, IsOptional, IsString } from 'class-validator';
+import { ApiPropertyOptional, OmitType } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
+import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto {
+export class UpdateUserDto extends OmitType(CreateUserDto, [
+    'login',
+    'password',
+]) {
     @ApiPropertyOptional({ description: 'User Password' })
-    @IsOptional()
     @IsString()
-    password?: string;
-
-    @ApiPropertyOptional({ description: 'User name' })
     @IsOptional()
+    password: string;
+
+    @ApiPropertyOptional({ description: 'User Password' })
     @IsString()
-    first_name?: string;
-
-    @ApiPropertyOptional({ description: 'User surname' })
     @IsOptional()
-    @IsString()
-    middle_name?: string;
-
-    @ApiPropertyOptional({ description: 'User familyname' })
-    @IsOptional()
-    @IsString()
-    last_name?: string;
-
-    @ApiPropertyOptional({ description: 'User birthday' })
-    @IsOptional()
-    @IsDateString()
-    bithday?: Date;
-
-    @ApiPropertyOptional({ description: 'User avatar' })
-    @IsOptional()
-    @IsString()
-    avatar?: string;
+    new_password: string;
 }
